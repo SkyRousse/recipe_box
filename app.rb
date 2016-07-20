5bea75b72fdcce79e72204e26ec9f7ef57b06103
Noah Yasskin and Sky Rousse <noah@>
2016-07-20 16:59:53 -0700

    add form to create ingredients and a route with failing spec, need to fix route

--- app.rb
+++ app.rb
@@ -9,6 +9,17 @@ end
 
 get('/recipes/:id') do
   @recipe = Recipe.find(params.fetch('id').to_i())
+  @ingredient
+  @ingredients = Ingredient.all()
+  erb(:recipe)
+end
+
+post('/ingredients') do
+  recipe_id = params.fetch('recipe_id')
+  @recipe = Recipe.find(recipe_id)
+  ingredient = params.fetch('ingredient')
+  @ingredients = Ingredient.all()
+  @ingredient = @recipe.ingredients.new(:id => nil, :ingredient => ingredient)
   erb(:recipe)
 end
 
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -17,3 +17,15 @@ describe('views a recipes route', {:type => :feature}) do
     expect(page).to have_content('YREA')
   end
 end
+
+describe('add ingredient route', {:type => :feature}) do
+  it('allows user to add an ingredient to a specific recipe') do
+    visit('/')
+    test_recipe = Recipe.create({:name => "Stabby Lambda"})
+    click_link('See All Recipes')
+    click_link('STABBY LAMBDA')
+    fill_in("ingredient", :with => "rocky mountain oyster")
+    click_button('Add Ingredient')
+    expect(page).to have_content("Rocky mountain oyster")
+  end
+end
