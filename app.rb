0ac3da714ef48136dfdca45dd1ea561652d1877b
Noah Yasskin and Sky Rousse <noah@>
2016-07-20 10:30:37 -0700

    add rakefile and database migrations and basic Ingredient and Recipe classes

--- app.rb
+++ app.rb
@@ -2,3 +2,14 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file}
+
+
+Create
+
+recipe.ingredients.push(new_ingredient)
+
+Recipe.new({:ingredient_ids => [ingredient1.id()]})
+
+ingredient1.recipees.new()
+
+reloader
